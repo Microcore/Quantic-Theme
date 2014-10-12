7c376ecee9a47c7e72263d0d8ddf73280a5e33a8
Joker Qyou <Joker.Qyou@gmail.com>
2014-10-12 21:59:08 +0800

    Use p instead of h3

--- Quantic/archive.php
+++ Quantic/archive.php
@@ -1,12 +1,12 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 <div class="col-sm-8 blog-main">
-    <h3 class="archive-title"><?php $this->archiveTitle(array(
+    <p class="bg-info archive-title"><?php $this->archiveTitle(array(
         'category'  =>  _t('分类 %s 下的文章'),
         'search'    =>  _t('包含关键字 %s 的文章'),
         'tag'       =>  _t('标签 %s 下的文章'),
         'author'    =>  _t('%s 发布的文章')
-    ), '', ''); ?></h3>
+    ), '', ''); ?></p>
     <?php if ($this->have()): ?>
     <?php while($this->next()): ?>
     <div class="blog-post" itemscope itemtype="http://schema.org/BlogPosting">
