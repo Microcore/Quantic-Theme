84b04db6a7b4ec25f16fa3a776eebf7e0d6e60c7
Joker Qyou <Joker.Qyou@gmail.com>
2014-10-12 15:57:15 +0800

    Init some codes

---
+++ Quantic/footer.php
@@ -0,0 +1,29 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+        </div><!-- end .row -->
+    </div>
+</div><!-- end #body -->
+
+<!-- Footer start -->
+<div class="blog-footer" id="footer" role="contentinfo">
+  <p>
+    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+    <?php _e('Powered by <a href="http://www.typecho.org">Typecho</a>.'); ?>
+  </p>
+</div>
+<!-- Footer end -->
+
+</body>
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//cdn.staticfile.org/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+    <script src="//cdn.staticfile.org/jquery.lazyload/1.9.1/jquery.lazyload.min.js" type="text/javascript"></script>
+    <script src="//cdn.staticfile.org/twitter-bootstrap/3.1.1/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//cdn.staticfile.org/jquery.qrcode/1.0/jquery.qrcode.min.js" type="text/javascript"></script>
+    <script src="//nooks.qiniudn.com/blog/boot.js" type="text/javascript"></script>
+  </body>
+</html>
+
+<?php $this->footer(); ?>
+</body>
+</html>
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+
+theme_dir=""
+
+echo "Theme will be installed to \`${theme_dir}\` "
+
+echo "Download theme pack... "
+wget -O quantic-theme.zip https://github.com/Microcore/Quantic-Theme/archive/master.zip
+
+echo "Unpack theme files... "
+unzip quantic-theme.zip
+rm quantic-theme.zip
+
+echo "Install theme files... "
+cd Quantic-Theme-master/
+mv Quantic "${theme_dir}"
+cd ../
+rm Quantic-Theme-master
+
+echo "Quantic theme installed. "
